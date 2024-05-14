3fbceb522f8d8c1a7cfeaea705e63bd5d6441682
Jaweriaawan <akhterwaheed470@gmail.com>
2024-05-14 18:59:52 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,7 @@
+// let home = ["roods","cables","cement"]
+// home = home.concat(["stairs","mud","rocks"])
+// console.log(home)
+
+let fruitchaat = ["cream","banana","apple","melon"]
+fruitchaat.push('kiwi')
+console.log(fruitchaat)
---
+++ main.js
@@ -0,0 +1,13 @@
+//jab hamen jawab yes or no m chahiye hota hai to type confirm hoti hai  
+import inquirer from "inquirer";
+let todos = [];
+let condition = (true);
+while (condition) {
+    let todolist = await inquirer.prompt([
+        { message: "what you want to add in your todos?", name: "todo", type: "input" },
+        { message: "do you want to add more ", type: "confirm", name: "addMore", default: "false" }
+    ]);
+    todos.push(todolist.todo);
+    condition = todolist.addMore;
+    console.log(todos);
+}
